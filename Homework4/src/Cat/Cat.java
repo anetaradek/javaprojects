efc4f5bfbe0ce7d9c309152879f4ecfcf4d4a495
anetaradek <aneta.j.radek@gmail.com>
2020-04-02 15:48:57 +0200

    Correction of indentation

--- Homework4/src/Cat/Cat.java
+++ Homework4/src/Cat/Cat.java
@@ -22,9 +22,9 @@ public void isHungry(){
         System.out.println(name+" is eating " + food);
         isItHungry = false;
         }
-        }
+    }
     public void play (String toy){
-    System.out.println(name+" is playing around with " + toy);
+        System.out.println(name+" is playing around with " + toy);
     }
     public void sleep (int hours){
         System.out.println(name + " is sleeping for " +hours+" hours");
