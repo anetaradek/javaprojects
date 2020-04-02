4be0e5cbe740793750c0c42f4eb4da9d2df1e924
anetaradek <aneta.j.radek@gmail.com>
2020-04-03 00:44:09 +0200

    Two variables brand and model instead of brandAndModel

--- Homework4/src/MobilePhone/MobilePhone.java
+++ Homework4/src/MobilePhone/MobilePhone.java
@@ -1,17 +1,18 @@
 package MobilePhone;
 
 public class MobilePhone {
-    public String brandAndModel;
-    public int batteryCapacity;
-    public int batteryAmount;
-    int freeMemory;
-    public int memory;
-    public  String appName;
-    public int appUsageTime;
-    public int appNeededMemory;
+    private String brand;
+    private String model;
+    private int batteryCapacity;
+    private int batteryAmount;
+    private int freeMemory;
+    int memory;
+
 
     public MobilePhone(String brandAndModel,int batteryCapacity,int memory){
-        this.brandAndModel = brandAndModel;
+        String[] brandAndModel1=brandAndModel.split(" ");
+        this.brand = brandAndModel1[0];
+        this.model = brandAndModel1[1];
         this.batteryCapacity = batteryCapacity;
         this.memory = memory;
         freeMemory = memory;
@@ -20,31 +21,32 @@ public class MobilePhone {
 
   public void charge(){
         batteryAmount = batteryCapacity;
-        System.out.println(brandAndModel +" has been charged. Remain battery capacity: "+batteryAmount+" mAh");
+        System.out.println(brand+" "+model +" has been charged. Remain battery capacity: "+batteryAmount+" mAh");
   }
 
   public void installApplication(String appName,int appNeededMemory) {
       if (freeMemory >= appNeededMemory) {
           freeMemory=freeMemory-appNeededMemory;
-          System.out.println("Application " + appName + " has been installed on " + brandAndModel + ". Remain memory: " + freeMemory + " MB");
+          System.out.println("Application " + appName + " has been installed on " + brand+" "+model + ". Remain memory: " + freeMemory + " MB");
       }else{
-          System.out.println("Application "+ appName + " can't be installed on "+ brandAndModel +". There are no enough memory");
+          System.out.println("Application "+ appName + " can't be installed on "+ brand+" "+model +". There are no enough memory");
       }
   }
 
   public void use(String appName,int appUsageTime){
         if(batteryAmount==0)
         {
-            System.out.println("Application "+ appName+" can't be run on "+brandAndModel+". Phone discharged");
+            System.out.println("Application "+ appName+" can't be run on "+brand+" "+model+". Phone discharged");
 
         } else if(batteryAmount>0) {
-            if (appUsageTime >= (batteryAmount / 100)) {
+            int batteryHours=batteryAmount/100;
+            if (appUsageTime >= batteryHours) {
                 batteryAmount = 0;
-                System.out.println("Application " + appName + " has been used for " + (batteryAmount / 100) + " hours on "
-                        + brandAndModel + ". Phone has been discharged.");
-            } else if (appUsageTime < (batteryAmount / 100)) {
+                System.out.println("Application " + appName + " has been used for " + batteryHours+ " hours on "
+                        + brand+" "+model + ". Phone has been discharged.");
+            } else{
                 batteryAmount = batteryAmount - (appUsageTime * 100);
-                System.out.println("Application" + appName + " has been ran for " + appUsageTime + " hours on " + brandAndModel +
+                System.out.println("Application" + appName + " has been ran for " + appUsageTime + " hours on " + brand+" "+model +
                         ". Remain battery capacity: " + batteryAmount + " mAh");
             }
         }
