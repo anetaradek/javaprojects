9ed4bd3736e8efb32dcd47433eb46f5fcbc10970
anetaradek <aneta.j.radek@gmail.com>
2020-04-28 16:04:02 +0200

    ArrayListVsLinkedList enhanced

--- Homework6/src/ArrayListVsLinkedList/ArrayListVsLinkedList.java
+++ Homework6/src/ArrayListVsLinkedList/ArrayListVsLinkedList.java
@@ -1,6 +1,5 @@
 package ArrayListVsLinkedList;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,40 +7,9 @@ import java.util.List;
 public class ArrayListVsLinkedList {
     public static void main(String[] args) {
 
-        long begginingTimeArrayList = System.currentTimeMillis();
-        List<Integer> arrayList= new ArrayList<>();
-        for (int i = 0; i <100_000; i++) {
-            if(i==0){arrayList.add(0,i); }
-            else arrayList.add(i);
-        }
-        long endingTimeArrayList = System.currentTimeMillis();
-        long totalLoopTimeArrayList = endingTimeArrayList-begginingTimeArrayList;
-        System.out.println("ArrayList total iteration time "+totalLoopTimeArrayList);
+        arrayList();
+        linkedList();
 
-        long begginingTimeLinkedList = System.currentTimeMillis();
-        List<Integer> linkedList= new LinkedList<>();
-        for (int i = 0; i <100_000; i++) {
-            if(i==0){linkedList.add(0,i); }
-            else linkedList.add(i);
-        }
-        long endingTimeLinkedList = System.currentTimeMillis();
-        long totalLoopTimeLinkedList = endingTimeLinkedList-begginingTimeLinkedList;
-        System.out.println("LinkedList total iteration time "+totalLoopTimeLinkedList);
-
-        long begginingTimeArrayList2 = System.currentTimeMillis();
-
-        for (int i = 0; i <100_000; i++) {
-            arrayList.get(i);
-        }
-        long endingTimeArrayList2 = System.currentTimeMillis();
-        long totalLoopTimeArrayList2 = endingTimeArrayList2-begginingTimeArrayList2;
-        System.out.println("ArrayList total iteration time "+totalLoopTimeArrayList2);
-        for (int i = 0; i <100_000; i++) {
-            linkedList.get(i);
-        }
-        long endingTimeLinkedList2 = System.currentTimeMillis();
-        long totalLoopTimeLinkedList2 = endingTimeArrayList-begginingTimeArrayList;
-        System.out.println("LinkedList total iteration time "+totalLoopTimeLinkedList2);
         System.out.println("Execution time to store elements in arrayList is longer because we save element\n" +
                 " via index, whereas in linkedList elements are connected to the previous and next element,\n" +
                 " so we just save our element as next element");
@@ -49,5 +17,48 @@ public class ArrayListVsLinkedList {
                 "by it's index, whereas in linkedList elements are connected to the previous and next element, \n" +
                 "so we get access to target element via first element through next elements until \n" +
                 "we get to our target element");
+    }
+
+    public static void arrayList() {
+        List<Integer> arrayList = new ArrayList<>();
+        long begginingTimeArrayList = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            if (i == 0) {
+                arrayList.add(0, i);
+            } else arrayList.add(i);
+        }
+        long endingTimeArrayList = System.currentTimeMillis();
+        long totalLoopTimeArrayList = endingTimeArrayList - begginingTimeArrayList;
+        System.out.println("ArrayList adding total iteration time " + totalLoopTimeArrayList);
+
+        long begginingTimeArrayList2 = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            arrayList.get(i);
+        }
+        long endingTimeArrayList2 = System.currentTimeMillis();
+        long totalLoopTimeArrayList2 = endingTimeArrayList2 - begginingTimeArrayList2;
+        System.out.println("ArrayList getting total iteration time " + totalLoopTimeArrayList2);
+    }
+
+    public static void linkedList () {
+        List<Integer> linkedList = new LinkedList<>();
+        long begginingTimeLinkedList = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            if (i == 0) {
+                linkedList.add(0, i);
+            } else linkedList.add(i);
+        }
+        long endingTimeLinkedList = System.currentTimeMillis();
+        long totalLoopTimeLinkedList = endingTimeLinkedList - begginingTimeLinkedList;
+        System.out.println("LinkedList adding total iteration time " + totalLoopTimeLinkedList);
+
+
+        long begginingTimeLinkedList2 = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            linkedList.get(i);
+        }
+        long endingTimeLinkedList2 = System.currentTimeMillis();
+        long totalLoopTimeLinkedList2 = endingTimeLinkedList2 - begginingTimeLinkedList2;
+        System.out.println("LinkedList getting total iteration time " + totalLoopTimeLinkedList2);
     }
 }
